12fa6e7f2c42baf97a39f332a355b630390b3449
Oscar <21753725a@iespoblenou.org>
2016-12-13 20:36:34 +0100

    initialize adapter

--- app/src/main/java/com/example/a21753725a/overwatchstats/MainActivityFragment.java
+++ app/src/main/java/com/example/a21753725a/overwatchstats/MainActivityFragment.java
@@ -1,15 +1,19 @@
 package com.example.a21753725a.overwatchstats;
 
+import android.content.Context;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivityFragment extends Fragment {
 
     View view;
-
+    Spinner platform;
+    Spinner region;
     public MainActivityFragment() {
     }
 
@@ -18,8 +22,19 @@ public class MainActivityFragment extends Fragment {
                              Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_main, container, false);
 
-
+        platform = spinnerAdapter(view.getContext(),platform,R.array.platform,R.id.region);
+        region = spinnerAdapter(view.getContext(),region,R.array.region,R.id.region);
 
         return view;
+    }
+
+    private Spinner spinnerAdapter(Context cont, Spinner spinner,int arrayRef, int layoutRef ) {
+
+        spinner = (Spinner) spinner.findViewById(R.id.region);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(cont, arrayRef, layoutRef);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        return spinner;
     }
 }
